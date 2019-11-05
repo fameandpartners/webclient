938600b0a15713165879a3b062d7286291478430
jinqingyang_branch <1063225587@qq.com>
2019-11-05 15:56:54 +0800

    package/env/webpack

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -14,9 +14,7 @@ const alias = require('./scripts/utils/alias');
 
 const IS_DEV = process.env.NODE_ENV === 'development';
 
-const devServerHost = process.env.HOST || '192.168.1.40';
-console.log("ttttttttttttttttttttttttttttttttttttttt");
-console.log(process.env.HOST);
+const devServerHost = process.env.HOST || 'localhost';
 const devServerPort = parseInt(process.env.PORT, 10) + 1 || 3003;
 
 const dotenv = getClientEnvironment('web');
