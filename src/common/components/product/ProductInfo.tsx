0440646d361a491a2a0e5b459f7323a644f77353
jinqingyang_branch <1063225587@qq.com>
2019-11-27 11:32:38 +0800

    前端隐藏quadpay

--- src/common/components/product/ProductInfo.tsx
+++ src/common/components/product/ProductInfo.tsx
@@ -263,7 +263,7 @@ class ProductInfo extends React.PureComponent<Props, State> {
           </p>
         )}
 
-        {product.siteVersionInfo.localisationCode === 'us' || product.siteVersionInfo.localisationCode === 'en-US' && (
+        {false && (product.siteVersionInfo.localisationCode === 'us' || product.siteVersionInfo.localisationCode === 'en-US') && (
           <p className="auxilary-info">
             <QuadpayTeaser total={total} />
           </p>
