743ebefc1d3f33aa32abb9ac554cc0e50e784644
jashrails <jash.mud@outlook.com>
2020-03-11 14:52:57 -0400

    Add url target blank field

--- src/common/components/cms/Item/LinkList.tsx
+++ src/common/components/cms/Item/LinkList.tsx
@@ -4,37 +4,46 @@ import { WysiwygText } from '@components/cms/CmsUtils';
 import BaseItem, { ItemSize, ItemTextSize, ItemTextAlign } from '@components/cms/Item/BaseItem';
 
 interface Props extends CmsElement {
-    links: Array<{
-        title: string;
-        url: string;
-    }>;
+  links: Array<{
+    title: string;
+    url: string;
+    urlTargetBlank?: boolean;
+  }>;
 }
 
 const ItemLinkList: React.SFC<Props> = ({ links }: Props) => {
-    return <BaseItem className="ItemLinkList" size={'Center'} mobileSize={'Center'} textAlignment={'Left'} textSize={'Medium'}>
-        <style jsx>{`
-            @import 'vars';
+  return (
+    <BaseItem className="ItemLinkList" size={'Center'} mobileSize={'Center'} textAlignment={'Left'} textSize={'Medium'}>
+      <style jsx>{`
+        @import 'vars';
 
-            ul {
-                @include text-style-h1;
-                font-size: 42px;
-            }
+        ul {
+          @include text-style-h1;
+          font-size: 42px;
+        }
 
-            li {
-                margin: 16px 0;
-                display: block;
+        li {
+          margin: 16px 0;
+          display: block;
 
-                a:hover,
-                a:focus {
-                    text-decoration: none;
-                    border-bottom: 2px solid $color-black;
-                }
-            }
-        `}</style>
-        <ul>
-            {links.map((x, i) => <li key={i}><a className="no-underline" href={x.url}>{x.title}</a></li>)}
-        </ul>
-    </BaseItem>;
+          a:hover,
+          a:focus {
+            text-decoration: none;
+            border-bottom: 2px solid $color-black;
+          }
+        }
+      `}</style>
+      <ul>
+        {links.map((x, i) => (
+          <li key={i}>
+            <a className="no-underline" href={x.url} target={x.urlTargetBlank === true ? '_blank' : '_self'} rel="noopener noreferrer">
+              {x.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </BaseItem>
+  );
 };
 
 export default ItemLinkList;
