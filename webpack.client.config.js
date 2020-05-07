9868b919b716c12ccfcaf13a8cb322f9b967a0f8
DapingWin10 <lidaping@graphicchina.com>
2020-05-07 12:48:11 +0800

    remove warnings option for uglify

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -256,7 +256,7 @@ module.exports = {
               },
               compress: {
                 //ecma: 5,
-                warnings: false,
+                //warnings: false,
                 // Disabled because of an issue with Uglify breaking seemingly valid code:
                 // https://github.com/facebook/create-react-app/issues/2376
                 // Pending further investigation:
