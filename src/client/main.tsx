97fad4258b355d0dd397fc124e3c651be8ef48d1
jinqingyang <jinqingyang@graphicchina.com>
2020-05-07 09:48:29 +0800

    临时提交

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -6,7 +6,7 @@ import queryString from 'query-string';
 
 import configureStore from '@common/rematch/index';
 
-//import '@hellohuman/array-exts';
+import '@hellohuman/array-exts';
 
 import App from '@containers/App/index';
 import LanguageProvider from '@containers/LanguageProvider/index';
