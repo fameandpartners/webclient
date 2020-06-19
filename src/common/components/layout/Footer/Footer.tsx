64fc8e0e9ac054e857a23c85295122d51d70a9d6
jinqingyang <jinqingyang@graphicchina.com>
2020-06-19 16:32:15 +0800

    修正删除的范围

--- src/common/components/layout/Footer/Footer.tsx
+++ src/common/components/layout/Footer/Footer.tsx
@@ -187,6 +187,13 @@ class Footer extends PureComponent<FooterProps> {
                     />)}
                     
                     <div className="copyright-spacer" />
+
+
+                    <div className="copyright-site-version">
+                        <div className="copyright text-secondary">
+                            <span>© {new Date().getFullYear()} Fame and Partners Inc.</span>
+                        </div>
+                    </div>
                 </div>
             </footer>
         );
