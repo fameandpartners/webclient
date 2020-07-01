a5a0e21a7ffb9223b88e6d7f18d92d04283e3a0b
liulimingTuhua <liuliming@graphicchina.com>
2020-07-01 18:06:15 +0800

    1.测试。

--- src/common/components/base/Curtain.tsx
+++ src/common/components/base/Curtain.tsx
@@ -46,12 +46,50 @@ class Curtain extends React.PureComponent<Props> {
     }
 
     public render() {
-        if (!this.el || !this.portalRoot) {
+      return null;
+      if (!this.el || !this.portalRoot) {
             return null;
         }
 
         return createPortal(
-            <React.Fragment/>,
+            <React.Fragment>
+                <FadeInOutTransition isVisible={this.props.isVisible}>
+                    { (styles) => (
+                        <div
+                            ref={this._root}
+                            className={classnames('curtain', { 'center-children': this.props.centerChildren })}
+                            style={{
+                                opacity: styles.opacity,
+                                ...this.props.style
+                            }}
+                        >
+                            <style jsx>{`
+                                @import 'vars';
+                                .curtain {
+                                    position: fixed;
+                                    visibility: hidden;
+                                    top: ${this.props.padNavbar ? 56 : 0}px;
+                                    right: 0;
+                                    bottom: 0;
+                                    left: 0;
+                                    background-color: rgba(0, 0, 0, 0.5);
+                                    z-index: $z-index-curtain;
+
+                                    &.center-children {
+                                        display: flex;
+                                        justify-content: center;
+                                        align-items: center;
+                                    }
+                                }
+                            `}</style>
+
+                            <BodyLock isVisible={this.props.isVisible} isHeader={this.props.padNavbar} onEscape={this.props.onClick}>
+                                {this.props.children}
+                            </BodyLock>
+                        </div>
+                    )}
+                </FadeInOutTransition>
+            </React.Fragment>,
             this.el,
         );
     }
